44038cc40108bb611bffc155eca0805e9b869620
Kousuke Ebihara <ebihara@tejimaya.com>
2008-12-26 10:01:55 +0000

    #3224:added the opAuthRegisterFormPCAddress class to adapt to r9710

---
+++ lib/form/opAuthRegisterFormPCAddress.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * opAuthRegisterFormPCAddress represents a form to register by PC E-mail Address.
+ *
+ * @package    OpenPNE
+ * @subpackage form
+ * @author     Kousuke Ebihara <ebihara@tejimaya.com>
+ */
+class opAuthRegisterFormPCAddress extends opAuthRegisterForm
+{
+}
