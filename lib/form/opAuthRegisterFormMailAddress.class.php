c18e01ef8f39fdcd08f275575ac791e096cd5558
Kousuke Ebihara <ebihara@tejimaya.com>
2009-01-05 06:34:26 +0000

    #3349:added opAuthRegisterFormMailAddress for handling registration by E-mail address. sfOpenPNEAuthForm_MailAddress is now useless for it

---
+++ lib/form/opAuthRegisterFormMailAddress.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * opAuthRegisterFormMailAddress represents a form to register by E-mail Address.
+ *
+ * @package    OpenPNE
+ * @subpackage form
+ * @author     Kousuke Ebihara <ebihara@tejimaya.com>
+ */
+class opAuthRegisterFormMailAddress extends opAuthRegisterForm
+{
+}
