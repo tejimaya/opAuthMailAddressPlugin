c9b9d24fe796bf1a4e6e1cdab634e878b732fcf2
Kousuke Ebihara <ebihara@tejimaya.com>
2009-01-07 05:01:08 +0000

    #3385:added the LICENSE file is a copy of the Apache License 2.0 and reference to it

--- lib/form/opAuthLoginFormMailAddress.class.php
+++ lib/form/opAuthLoginFormMailAddress.class.php
@@ -1,6 +1,14 @@
 <?php
 
 /**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+/**
  * opAuthLoginFormMailAddress represents a form to login by one's E-mail address.
  *
  * @package    OpenPNE
