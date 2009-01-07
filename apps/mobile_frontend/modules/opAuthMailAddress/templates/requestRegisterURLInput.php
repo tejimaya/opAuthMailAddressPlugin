541345ae46839e7d46efca773c84eeca84fc0253
Kousuke Ebihara <ebihara@tejimaya.com>
2009-01-07 12:23:48 +0000

    #3388:applied r10027 to opAuthMailAddressPlugin

--- apps/mobile_frontend/modules/opAuthMailAddress/templates/requestRegisterURLInput.php
+++ apps/mobile_frontend/modules/opAuthMailAddress/templates/requestRegisterURLInput.php
@@ -1,5 +1,5 @@
 <p>ﾒｰﾙｱﾄﾞﾚｽを入力してください。</p>
-<p>入力されたﾒｰﾙｱﾄﾞﾚｽ宛に <?php echo opConfig::get('sns_name') ?> の招待状が送信されます。</p>
+<p>入力されたﾒｰﾙｱﾄﾞﾚｽ宛に <?php echo $op_config['sns_name'] ?> の招待状が送信されます。</p>
 <form action="<?php echo url_for('opAuthMailAddress/requestRegisterURL') ?>" method="post">
 <table>
 <?php echo $form ?>
