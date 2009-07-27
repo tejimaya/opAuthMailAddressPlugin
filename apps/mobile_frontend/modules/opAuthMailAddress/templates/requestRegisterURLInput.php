cebe43a1af11ce7ed425d14b57cf77eaee856794
Kousuke Ebihara <ebihara@tejimaya.com>
2009-07-27 07:00:21 +0000

    added some customizes point (experimental)

--- apps/mobile_frontend/modules/opAuthMailAddress/templates/requestRegisterURLInput.php
+++ apps/mobile_frontend/modules/opAuthMailAddress/templates/requestRegisterURLInput.php
@@ -1,5 +1,7 @@
+<?php include_customizes('inputForm', 'top') ?>
 <p>ﾒｰﾙｱﾄﾞﾚｽを入力してください。</p>
 <p>入力されたﾒｰﾙｱﾄﾞﾚｽ宛に <?php echo $op_config['sns_name'] ?> の招待状が送信されます。</p>
+<?php include_customizes('inputForm', 'formTop') ?>
 <form action="<?php echo url_for('opAuthMailAddress/requestRegisterURL') ?>" method="post">
 <table>
 <?php echo $form ?>
@@ -7,4 +9,6 @@
 <td><input type="submit" value="送信" /></td>
 </tr>
 </table>
+<?php include_customizes('inputForm', 'formBottom') ?>
 </form>
+<?php include_customizes('inputForm', 'bottom') ?>
