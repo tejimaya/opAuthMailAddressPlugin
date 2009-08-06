33658e612df6d125bdeabb5b1088e30fe4ee6d9a
Kousuke Ebihara <ebihara@tejimaya.com>
2009-08-06 05:53:02 +0000

    fixed that requestRegisterURL doesn't render global errors

--- apps/mobile_frontend/modules/opAuthMailAddress/templates/requestRegisterURLInput.php
+++ apps/mobile_frontend/modules/opAuthMailAddress/templates/requestRegisterURLInput.php
@@ -3,6 +3,9 @@
 <p>入力されたﾒｰﾙｱﾄﾞﾚｽ宛に <?php echo $op_config['sns_name'] ?> の招待状が送信されます。</p>
 <?php include_customizes('inputForm', 'formTop') ?>
 <form action="<?php echo url_for('opAuthMailAddress/requestRegisterURL') ?>" method="post">
+<?php if ($form->hasGlobalErrors()): ?>
+<?php echo $form->renderGlobalErrors() ?><br>
+<?php endif; ?>
 <table>
 <?php echo $form ?>
 <tr>
