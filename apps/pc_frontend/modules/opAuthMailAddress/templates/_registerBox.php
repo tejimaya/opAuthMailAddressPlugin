236b6a54545055df47d2bf178a780e9d3be1a75b
Masato Nagasawa <nagasawa@tejimaya.com>
2011-05-02 19:09:07 +0900

    added the translation for the registration page (fixes #1770)

--- apps/pc_frontend/modules/opAuthMailAddress/templates/_registerBox.php
+++ apps/pc_frontend/modules/opAuthMailAddress/templates/_registerBox.php
@@ -1,7 +1,7 @@
 <?php op_include_parts('ButtonBox', 'opAuthMailAddressPluginRegisterBox', array(
-  'title'  => 'メールアドレスで登録する',
-  'body'   => '以下のボタンをクリックすると、招待されたメールアドレスで登録をおこないます。',
-  'button' => 'プロフィール入力ページへ',
+  'title'  => __('Registration with your e-mail address'),
+  'body'   => __('You can go to the registration page by clicking the button bellow.'),
+  'button' => __('Go to the registration page'),
   'url'    => url_for($sf_user->getRegisterInputAction()),
   'method' => 'get',
 )) ?>
