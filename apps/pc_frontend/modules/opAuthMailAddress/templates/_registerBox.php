c44c7c4e0e26f9d28ff8d9b09e31ffc515e862a2
Masato Nagasawa <nagasawa@tejimaya.com>
2011-05-24 15:06:00 +0900

    fixed spelling error (fixes #1770)

--- apps/pc_frontend/modules/opAuthMailAddress/templates/_registerBox.php
+++ apps/pc_frontend/modules/opAuthMailAddress/templates/_registerBox.php
@@ -1,6 +1,6 @@
 <?php op_include_parts('ButtonBox', 'opAuthMailAddressPluginRegisterBox', array(
   'title'  => __('Registration with your e-mail address'),
-  'body'   => __('You can go to the registration page by clicking the button bellow.'),
+  'body'   => __('You can go to the registration page by clicking the button below.'),
   'button' => __('Go to the registration page'),
   'url'    => url_for($sf_user->getRegisterInputAction()),
   'method' => 'get',
