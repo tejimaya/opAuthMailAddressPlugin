831fe849239ed00fa09ff0f5349a759de8090b70
Maki Takahashi <martini2002jp@gmail.com>
2011-09-06 20:27:19 +0900

    (fixes #1668) added the validation for application and e-mail

--- apps/mobile_frontend/modules/opAuthMailAddress/templates/_registerBox.php
+++ apps/mobile_frontend/modules/opAuthMailAddress/templates/_registerBox.php
@@ -1,3 +1,6 @@
+<?php if (!$mobileAddressPre): ?>
+<?php echo __('Can access this registration URL with pc only.') ?>
+<?php else: ?>
 <?php op_include_parts('ButtonBox', 'opAuthMailAddressPluginRegisterBox', array(
   'title'  => __('Registration with your e-mail address'),
   'body'   => __('You can go to the registration page by clicking the button below.'),
@@ -5,3 +8,4 @@
   'url'    => url_for($sf_user->getRegisterInputAction()),
   'method' => 'get',
 )) ?>
+<?php endif; ?>
